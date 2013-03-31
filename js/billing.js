a03f7cc962e895ebf54c90b037d48e523313a970
Andrew Rivett <veggiefrog@gmail.com>
2013-03-30 22:03:21 -0400

    Added more advanced billing editing features for sysadmin

--- js/billing.js
+++ js/billing.js
@@ -5,37 +5,29 @@ function ciniki_sysadmin_billing() {
     this.businesses = null;
 
     this.init = function() {
-		
 		//
 		// This panel will list the businesses in the system, their status, and subscription status.
 		//
         this.businesses = new M.panel('Billing',
             'ciniki_sysadmin_billing', 'businesses',
            	'mc', 'medium', 'sectioned', 'ciniki.sysadmin.billing.businesses');
-		this.businesses.sections = {
-			'_':{'label':'', 'type':'simplegrid', 'num_cols':5, 'sortable':'yes',
-				'headerValues':['Business', 'Status', 'Subscription', 'Monthly', 'Last Payment'],
-				'sortTypes':['text','text','text','number','date'],
-				'cellClasses':['', 'aligncenter', 'aligncenter', '', ''],
-				},
-			};
-		this.businesses.sectionData = function(s) { return this.data; }
-        this.businesses.cellValue = function(s, i, col, d) { 
-			switch(col) {
-				case 0: return d.business.name;
+		this.businesses.sections = {};
+		this.businesses.sectionData = function(s) { return this.data[s]; }
+        this.businesses.cellValue = function(s, i, j, d) { 
+			switch(j) {
+				case 0: return '<span class="maintext">' + d.business.name + '</span><span class="subtext">' + d.business.payment_type + '</span>';
 				case 1: return d.business.business_status;
-				case 2: return d.business.status;
-				case 3: return d.business.monthly;
-				case 4: return d.business.last_payment_date;
+				case 2: return d.business.monthly;
+				case 3: return '<span class="maintext">' + d.business.last_payment_date + '</span><span class="subtext">' + d.business.paid_until + '</span>';
 			}
 			return '';
 		};
 		this.businesses.rowFn = function(s, i, d) { 
-			return 'M.startApp(\'ciniki.sysadmin.business\',null,\'M.ciniki_sysadmin_billing.showBusinesses();\',\'mc\',{\'id\':\'' + d.business.id + '\'});'; 
+//			return 'M.startApp(\'ciniki.sysadmin.business\',null,\'M.ciniki_sysadmin_billing.showBusinesses();\',\'mc\',{\'id\':\'' + d.business.id + '\'});'; 
+			return 'M.startApp(\'ciniki.businesses.billing\',null,\'M.ciniki_sysadmin_billing.showBusinesses();\',\'mc\',{\'business_id\':\'' + d.business.id + '\'});'; 
 		};
         this.businesses.noData = function() { return 'ERROR - No sysadmins'; }
         this.businesses.addClose('Back');
-
     }   
 
     this.start = function(cb, appPrefix) {
@@ -58,9 +50,19 @@ function ciniki_sysadmin_billing() {
 			M.api.err(rsp);
 			return false;
 		}
-		this.businesses.data = rsp.businesses;
+//		this.businesses.data = rsp.statuses;
+		this.businesses.data = {};
+		this.businesses.sections = {};
+		for(i in rsp.statuses) {
+			this.businesses.data[i] = rsp.statuses[i].status.businesses;
+			this.businesses.sections[i] = {'label':rsp.statuses[i].status.status, 
+				'type':'simplegrid', 'num_cols':4, 'sortable':'yes',
+				'headerValues':['Business', 'Status', 'Monthly', 'Last Payment'],
+				'sortTypes':['text','text','text','number','date'],
+				'cellClasses':['multiline', 'aligncenter', 'aligncenter', 'multiline', ''],
+			};
+		}
 		this.businesses.refresh();
 		this.businesses.show(cb);
 	};
-
 }
