8f8819a508b9045af3636f1c4dffedaea45432b4
Andrew Rivett <veggiefrog@gmail.com>
2014-03-25 00:01:51 -0400

    Added import ciniki.info

--- ui/main.js
+++ ui/main.js
@@ -44,9 +44,10 @@ function ciniki_sysadmin_main() {
 				'code':{'label':'Code Versions', 'fn':'M.startApp(\'ciniki.sysadmin.codeversions\', null, \'M.ciniki_sysadmin_main.menu.show();\');'},
 				'modules':{'label':'Module usage', 'fn':'M.startApp(\'ciniki.sysadmin.modules\',null,\'M.ciniki_sysadmin_main.menu.show();\');'},
 				}},
-//			'migration':{'label':'Migration', 'list':{
+			'migration':{'label':'Migration', 'list':{
+				'infoimport':{'label':'ciniki.info Import', 'fn':'M.ciniki_sysadmin_main.infoimport();'},
 //				'phonesmove':{'label':'Move Phones', 'fn':'M.ciniki_sysadmin_main.phonesmove();'},
-//				}},
+				}},
 //			'documentation':{'label':'Documentation', 'list':{
 //				'modules':{'label':'Modules', 'fn':'M.startApp(\'ciniki.documentation.modules\', null, \'M.ciniki_sysadmin_main.menu.show();\');'},
 //				'errorcodes':{'label':'Error Codes', 'fn':'M.startApp(\'ciniki.documentation.errorcodes\', null, \'M.ciniki_sysadmin_main.menu.show();\');'},
@@ -87,8 +88,18 @@ function ciniki_sysadmin_main() {
 		});
 	};
 
-	this.phonesmove = function() {
-		var rsp = M.api.getJSONCb('ciniki.customers.phonesMove', {}, function(rsp) {
+//	this.phonesmove = function() {
+//		var rsp = M.api.getJSONCb('ciniki.customers.phonesMove', {}, function(rsp) {
+//			if( rsp.stat != 'ok' ) {
+//				M.api.err(rsp);
+//				return false;
+//			}
+//			alert('done');
+//		});
+//	};
+
+	this.infoimport = function() {
+		var rsp = M.api.getJSONCb('ciniki.info.import', {}, function(rsp) {
 			if( rsp.stat != 'ok' ) {
 				M.api.err(rsp);
 				return false;
