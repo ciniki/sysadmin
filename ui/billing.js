faf01ca9842ab29d5b1d3518445b4dfe9b76d321
Andrew Rivett <veggiefrog@gmail.com>
2014-03-27 22:17:35 -0400

    Updated billing panel

--- ui/billing.js
+++ ui/billing.js
@@ -12,7 +12,7 @@ function ciniki_sysadmin_billing() {
             'ciniki_sysadmin_billing', 'businesses',
            	'mc', 'medium', 'sectioned', 'ciniki.sysadmin.billing.businesses');
 		this.businesses.sections = {};
-		this.businesses.sectionData = function(s) { return this.data[s]; }
+		this.businesses.sectionData = function(s) { return this.data[s].businesses; }
         this.businesses.cellValue = function(s, i, j, d) { 
 			if( d.business.status == 'Trial' && j == 1 ) {
 				return '<span class="maintext">' + d.business.business_status + '</span><span class="subtext">' + d.business.trial_remaining + '</span>';
@@ -20,9 +20,23 @@ function ciniki_sysadmin_billing() {
 			switch(j) {
 				case 0: return '<span class="maintext">' + d.business.name + '</span><span class="subtext">' + d.business.payment_type + ' - ' + d.business.payment_frequency + '</span>';
 				case 1: return d.business.business_status;
-				case 2: return d.business.monthly;
+				case 2: return d.business.monthly + '/' + d.business.yearly;
 				case 3: return '<span class="maintext">' + d.business.last_payment_date + '</span><span class="subtext">' + d.business.paid_until + '</span>';
 			}
+			return '';
+		};
+		this.businesses.footerValue = function(s, i, d) {
+//			if( this.data.totals != null ) {
+				switch(i) {
+					case 0: return '';
+					case 1: return '';
+					case 2: return this.data[s].monthly + '/' + this.data[s].yearly;
+					case 3: return '';
+				}
+//			}
+		};
+		this.businesses.footerClass = function(s, i, d) {
+			if( i == 2 ) { return 'alignright'; }
 			return '';
 		};
 		this.businesses.rowFn = function(s, i, d) { 
@@ -57,7 +71,9 @@ function ciniki_sysadmin_billing() {
 			p.data = {};
 			p.sections = {};
 			for(i in rsp.statuses) {
-				p.data[i] = rsp.statuses[i].status.businesses;
+				p.data[i] = rsp.statuses[i].status;
+//				p.data[i].monthly = rsp.statuses[i].status.monthly;
+//				p.data[i].yearly = rsp.statuses[i].status.yearly;
 				p.sections[i] = {'label':rsp.statuses[i].status.status, 
 					'type':'simplegrid', 'num_cols':4, 'sortable':'yes',
 					'headerValues':['Business', 'Status', 'Monthly', 'Last Payment'],
