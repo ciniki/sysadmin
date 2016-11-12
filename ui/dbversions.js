d81c993f362ae14e8541356ee525129d958f2fd5
Andrew Rivett <veggiefrog@gmail.com>
2016-11-12 08:37:27 -0500

    Updated dbversions.js to new format

--- ui/dbversions.js
+++ ui/dbversions.js
@@ -1,71 +1,38 @@
 //
 //
 function ciniki_sysadmin_dbversions() {
-    this.tables = null;
-
-    this.init = function() {
-        //
-        // Create the panel for the information
-        //
-        this.tables = new M.panel('Table Versions',
-            'ciniki_sysadmin_dbversions', 'tables',
-            'mc', 'medium', 'sectioned', 'ciniki.sysadmin.db_table_versions');
-        this.tables.sections = {
-            '_':{'label':'', 'type':'simplegrid', 'num_cols':3, 
-                'headerValues':['Table', 'Database', 'Current'],
-                },
-            };
-        this.tables.sectionData = function(s) { return this.data; }
-        
-        //
-        // Load the data into the this.tables.data field
-        //
-        this.tables.cellClass = function(s, i, j, d) {
-            if( d.database_version != d.schema_version ) {
-                return 'alert';
-            }
-            return null;
+    //
+    // Create the panel for the information
+    //
+    this.dbtables = new M.panel('Table Versions', 'ciniki_sysadmin_dbversions', 'dbtables', 'mc', 'medium', 'sectioned', 'ciniki.sysadmin.dbversions.dbtables');
+    this.dbtables.sections = {
+        '_':{'label':'', 'type':'simplegrid', 'num_cols':3, 
+            'headerValues':['Table', 'Database', 'Current'],
+            },
+        };
+    this.dbtables.sectionData = function(s) { return this.data; }
+    this.dbtables.cellClass = function(s, i, j, d) {
+        if( d.database_version != d.schema_version ) {
+            return 'alert';
         }
-        this.tables.cellValue = function(s, i, j, d) {
-            switch(j) {
-                case 0: return i;
-                case 1: return this.data[i].database_version;
-                case 2: return this.data[i].schema_version;
-            }
-            return '';
+        return null;
+    }
+    this.dbtables.cellValue = function(s, i, j, d) {
+        switch(j) {
+            case 0: return i;
+            case 1: return this.data[i].database_version;
+            case 2: return this.data[i].schema_version;
         }
-        this.tables.addButton('update', 'Upgrade', 'M.ciniki_sysadmin_dbversions.upgrade();');
-        this.tables.addClose('Back');
+        return '';
     }
-
-    this.start = function(cb, appPrefix) {
-        //
-        // Create the app container if it doesn't exist, and clear it out
-        // if it does exist.
-        //
-        var appContainer = M.createContainer(appPrefix, 'ciniki_sysadmin_dbversions', 'yes');
-        if( appContainer == null ) {
-            alert('App Error');
-            return false;
-        } 
-
-
-        this.showTables(cb);
-    }
-
-    this.showTables = function(cb) {
-        //
-        // Get the detail for the user.  Do this for each request, to make sure
-        // we have the current data.  If the user switches businesses, then we
-        // want this data reloaded.
-        //
-        var rsp = M.api.getJSONCb('ciniki.core.checkDbTableVersions', {}, function(rsp) {
+    this.dbtables.open = function(cb) {
+        M.api.getJSONCb('ciniki.core.checkDbTableVersions', {}, function(rsp) {
             if( rsp.stat != 'ok' ) {
                 M.api.err(rsp);
                 return false;
             }
             var count = 0;
-            var p = M.ciniki_sysadmin_dbversions.tables;
+            var p = M.ciniki_sysadmin_dbversions.dbtables;
             p.data = {};
             //
             // Add the tables which need upgrading first, so they appear at the top of the list
@@ -88,15 +55,29 @@ function ciniki_sysadmin_dbversions() {
             p.show(cb);
         });
     }
-
-    this.upgrade = function() {
-        var rsp = M.api.getJSONCb('ciniki.core.upgradeDb', {}, function(rsp) {
+    this.dbtables.upgrade = function() {
+        M.api.getJSONCb('ciniki.core.upgradeDb', {}, function(rsp) {
             if( rsp.stat != 'ok' ) {
                 alert("Error: #" + rsp.err.code + ' - ' + rsp.err.msg);
                 return false;
             }
-            M.ciniki_sysadmin_dbversions.showTables();
+            M.ciniki_sysadmin_dbversions.dbtables.open();
         });
     }
-}
+    this.dbtables.addButton('update', 'Upgrade', 'M.ciniki_sysadmin_dbversions.dbtables.upgrade();');
+    this.dbtables.addClose('Back');
 
+    this.start = function(cb, appPrefix) {
+        //
+        // Create the app container if it doesn't exist, and clear it out
+        // if it does exist.
+        //
+        var appContainer = M.createContainer(appPrefix, 'ciniki_sysadmin_dbversions', 'yes');
+        if( appContainer == null ) {
+            alert('App Error');
+            return false;
+        } 
+
+        this.dbtables.open(cb);
+    }
+}
